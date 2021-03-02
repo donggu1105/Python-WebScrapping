61c3bb3db472670d7bea709f3844d88b0f216306
donggu1105 <kbh11051@gmail.com>
2021-03-02 16:12:42 +0900

    User-Agent

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-# import requests
-#
-# res = requests.get("https://naver.com")
-# res.raise_for_status()
-#
-# with open("mynaver.html", "w", encoding="utf8") as f :
-#     f.write(res.text)
+import requests
+header = {"User-Agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.192 Safari/537.36"}
+res = requests.get("https://naver.com", header=header)
+res.raise_for_status()
+
+with open("mynaver.html", "w", encoding="utf8") as f :
+    f.write(res.text)
